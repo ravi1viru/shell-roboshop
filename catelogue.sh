e45e0c4efecea7466dfb745f64741401bbeff100
Raveendra Meesala <meesala10@gmail.com>
2026-09-16 23:00:41 +0530

    message

--- catelogue.sh
+++ catelogue.sh
@@ -1,93 +1,94 @@
-
-
-
 #!/bin/bash
 
-
-USERID=$(id -u)
+# Colors
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# Variables
+USERID=$(id -u)
 LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
+# Ensure log folder exists
 mkdir -p $LOG_FOLDER
-echo " script starting date: $(date) " | tee -a $LOG_FILE
+echo "Script starting date: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]
-    then
-         echo -e "$R Error: please run the script with root user $N" | tee -a $LOG_FILE
-         exit 1
-    else
-         echo " You are running with root access" 
+# Root user check
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Error: please run the script with root user $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-VALIDATE(){
-
-    if [ $1 -eq 0 ]
-       then 
-           echo -e " $2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
-       else
-           echo " $2 IS ... $R FAILURE $N" | tee -a $LOG_FILE
-           exit 1
+# Validation function
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 .... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 .... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
 
-dnf module disable nodejs -y
-VALIDATE $? "disable nodejs default version"
+# NodeJS setup
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable NodeJS default version"
 
-dnf module enable nodejs:20 -y
-VALIDATE $? "enable nodejs 20 version"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable NodeJS 20 version"
 
-dnf install nodejs -y
-VALIDATE $? "install nodejs 20 version"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install NodeJS 20 version"
 
-id roboshop
-if [ $? -ne 0 ]
-then
-
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-VALIDATE $? " create a systeam user roboshop"
-
+# Create roboshop user if not exists
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Create system user roboshop"
 else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+    echo -e "System user roboshop already exists ... $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-mkdir -p /app 
-VALIDATE $? " create app directory"
+# Create app directory
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "Create /app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-VALIDATE $? " download catelougue component"
+# Download and extract catalogue
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Download catalogue component"
 
-cd /app 
-unzip /tmp/catalogue.zip
-VALIDATE $? " unzip catelougue component"
+cd /app
+unzip -o /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzip catalogue component"
 
-cd /app 
-npm install 
-VALIDATE $? " install dependncies"
+npm install &>>$LOG_FILE
+VALIDATE $? "Install NodeJS dependencies"
 
-cp $SCRIPT_DIR=$PWD/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? " copy catalogue servives"
+# Copy systemd service file
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "Copy catalogue service file"
 
-systemctl daemon-reload
-VALIDATE $? " relod new services"
+# Start service
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Reload systemd"
 
-systemctl enable catalogue 
-VALIDATE $? " enable new services"
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue service"
 
-systemctl start catalogue
-VALIDATE $? " start new services"
+systemctl start catalogue &>>$LOG_FILE
+VALIDATE $? "Start catalogue service"
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? " coping load the data"
+# MongoDB setup
+cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "Copy MongoDB repo file"
 
-dnf install mongodb-mongosh -y
-VALIDATE $? " install mongodb"
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Install MongoDB client"
 
-mongosh --host mongodb.leardevops.online </app/db/master-data.js
-VALIDATE $? " load the master data"
+mongosh --host mongodb.leardevops.online </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "Load master data into MongoDB"
