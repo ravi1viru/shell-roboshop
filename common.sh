a5e2fa547c2d28d7b956bce2502d7c4641b2a27d
Raveendra Meesala <meesala10@gmail.com>
2026-09-18 01:04:12 +0530

    message

--- common.sh
+++ common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Colors
 R="\e[31m"
 G="\e[32m"
@@ -7,31 +9,29 @@ N="\e[0m"
 # Variables
 USERID=$(id -u)
 LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" .sh)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 # Ensure log folder exists
-mkdir -p $LOG_FOLDER
-echo "Script starting date: $(date)" | tee -a $LOG_FILE
+mkdir -p "$LOG_FOLDER"
 
-# Root user check
-check_root(){
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Error: please run the script with root user $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
+# Root user check function
+check_root() {
+    if [ "$USERID" -ne 0 ]; then
+        echo -e "$R Error: Please run the script with root user $N" | tee -a "$LOG_FILE"
+        exit 1
+    else
+        echo "You are running with root access" | tee -a "$LOG_FILE"
+    fi
 }
 
 # Validation function
 VALIDATE() {
-    if [ $1 -eq 0 ]; then
-        echo -e "$2 .... $G SUCCESS $N" | tee -a $LOG_FILE
+    if [ -n "$1" ] && [ "$1" -eq 0 ]; then
+        echo -e "$2 .... $G SUCCESS $N" | tee -a "$LOG_FILE"
     else
-        echo -e "$2 .... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 .... $R FAILURE $N" | tee -a "$LOG_FILE"
         exit 1
     fi
 }
