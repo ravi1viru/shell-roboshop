683e87102f945d1acc39ab2bee9bdbd4d905def0
Raveendra Meesala <meesala10@gmail.com>
2026-09-18 00:54:34 +0530

    message

--- mongodb.sh
+++ mongodb.sh
@@ -1,37 +1,7 @@
 #!/bin/bash
 
+source ./common.sh
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-
-mkdir -p $LOG_FOLDER
-echo " script starting date: $(date) " | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-    then
-         echo -e "$R Error: please run the script with root user $N" | tee -a $LOG_FILE
-         exit 1
-    else
-         echo " You are running with root access" 
-fi
-
-VALIDATE(){
-
-    if [ $1 -eq 0 ]
-       then 
-           echo -e " $2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
-       else
-           echo " $2 IS ... $R FAILURE $N" | tee -a $LOG_FILE
-           exit 1
-    fi
-}
 
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copig mangodb repo" 
